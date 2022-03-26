5837e6df041c8c39d9452993694aed835c7fabc0
Marcus <marcusclemmons1@gmail.com>
2022-03-25 20:17:28 -0400

    done

---
+++ src/PlayingCardList.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { useAxios } from "./hooks";
+import PlayingCard from "./PlayingCard";
+import { cardDisplay } from "./helpers";
+import "./PlayingCardList.css";
+
+/* Renders a list of playing cards.
+ * Can also add a new card at random. */
+function CardTable() {
+  const [cards, pickCard, removeCards] = useAxios(
+      'cards',
+      "https://deckofcardsapi.com/api/deck/new/draw/"
+    );
+  
+  
+  return (
+    <div className="PlayingCardList">
+      <h3>Pick a card, any card!</h3>
+      <div>
+        <button onClick={() => pickCard(cardDisplay)}>Add a playing card! </button>
+        <button onClick={removeCards}>Empty</button>
+      </div>
+      <div className="PlayingCardList-card-area">
+        {cards.map(card => (
+          <PlayingCard key={card.id} front={card.image} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+CardTable.defaultProps = {};
+
+export default CardTable;
---
+++ src/helpers.js
@@ -0,0 +1,32 @@
+import { v4 as uuid } from "uuid";
+
+
+
+/* Select a random element from values array. */
+function choice(values) {
+  const randIdx = Math.floor(Math.random() * values.length);
+  return values[randIdx];
+}
+
+function cardDisplay(data) {
+  return {
+    image: data.cards[0].image,
+    id: uuid()
+  };
+}
+
+
+function pokemonDisplay(data) {
+  return {
+    id: uuid(),
+    front: data.sprites.front_default,
+    back: data.sprites.back_default,
+    name: data.name,
+    stats: data.stats.map(stat => ({
+      value: stat.base_stat,
+      name: stat.stat.name
+    }))
+  };
+}
+
+export { choice, cardDisplay, pokemonDisplay };
